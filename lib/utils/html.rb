8a374b38d1e8b727f365b6ce5edf5424eb1a0c7d
davor <vorgrimmlerdavid@gmx.de>
2012-12-13 15:27:30 +0100

    Update lib/utils/html.rb
    Changed color ;-)

--- lib/utils/html.rb
+++ lib/utils/html.rb
@@ -42,7 +42,7 @@ module OpenTox
     html += "<h3>Description</h3><pre><p>"+description.link_urls+"</p></pre>" if description
     html += "<h3>Related links</h3><pre><p>"+related_links.link_urls+"</p></pre>" if related_links
     html += "<h3>Content</h3>" if description || related_links
-    html += "<pre><p style=\"padding:15px; border:10px solid \#5D308A\">"
+    html += "<pre><p style=\"padding:15px; border:10px solid \#B9DCFF\">"
     html += "<img src=\"data:image/png;base64,#{Base64.encode64(png_image)}\">\n" if png_image
     html += text.link_urls
     html += "</p></pre></body></html>"
